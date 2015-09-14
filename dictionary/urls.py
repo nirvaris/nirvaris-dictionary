15f477bb13dbd89601e00b38cadf4ac4f84735e6
JB <juliano.binder@gmail.com>
2015-09-14 15:52:48 +0100

    urls

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from .views import WordEntryView, SearchView
 
 urlpatterns = [
-    url(r'^(?P<tags>.*)$', WordEntryView.as_view()),
     url(r'^search$', SearchView.as_view()),
+    url(r'^(?P<tags>.*)$', WordEntryView.as_view()),
+
 
 ]
