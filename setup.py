364bbc5fa80194dc056cb6ecb46123c000138f9b
Cristiano Kurt Ritzke <cristianokr@Cristianos-MacBook-Pro.local>
2015-10-09 10:23:48 -0300

    version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='nirvaris-dictionary',
-    version='0.3',
+    version='0.4',
     packages=['dictionary'],
     include_package_data=True,
     license='MIT License',  # example license
