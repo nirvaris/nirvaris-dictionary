f91ba3fa9751d449267eb52acf8609edf5ab33ce
JB <juliano.binder@gmail.com>
2016-01-07 16:05:01 +0000

    NV_DICTIONARY_URL

--- dictionary/templatetags/search_form_tag.py
+++ dictionary/templatetags/search_form_tag.py
@@ -6,9 +6,15 @@ from django.template.loader import render_to_string
 
 from ..forms import SearchForm
 
+NV_DICTIONARY_URL = 'dictionary'
+
+if hasattr(settings, 'NV_DICTIONARY_URL'):
+    if settings.NV_DICTIONARY_URL:
+        NV_DICTIONARY_URL = settings.NV_DICTIONARY_URL
+
 register = template.Library()
 
 @register.inclusion_tag('tag-search-form.html')
 def search_form_tag():
     form = SearchForm()
-    return {'search_form':form}
+    return {'search_form':form, 'dictionary_url': NV_DICTIONARY_URL}
--- dictionary/urls.py
+++ dictionary/urls.py
@@ -7,13 +7,14 @@ from .views import WordEntryView, SearchView, UploadCSVView, DemoSearchFormView,
 
 
 urlpatterns = [
-    url(r'^profile/', include('n_profile.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^profile/', include('n_profile.urls')),
     url(r'^search-form-tag$', DemoSearchFormView.as_view(), name='search-form-tag'),
     url(r'^search$', SearchView.as_view(), name='search'),
     url(r'^upload-csv$', login_required(UploadCSVView.as_view()), name='upload-csv'),
     url(r'^upload-tag-csv$', login_required(UploadTagCSVView.as_view()), name='upload-tag-csv'),
     url(r'^upload-language-csv$', login_required(UploadLanguageCSVView.as_view()), name='upload-language-csv'),
-    url(r'^download-log$', login_required(DownloadImportLogView.as_view()), name='download-log'),        
+    url(r'^download-log$', login_required(DownloadImportLogView.as_view()), name='download-log'),
+
     url(r'^(?P<tags>.*)$', WordEntryView.as_view()),
 ]
