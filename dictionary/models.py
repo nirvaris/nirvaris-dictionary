c862e125e6beb12197cdcffcb1d712ab578f375f
JB <juliano.binder@gmail.com>
2016-01-11 10:37:48 +0000

    logging heroku

--- dictionary/models.py
+++ dictionary/models.py
@@ -24,18 +24,18 @@ class WordClass(models.Model):
     name = models.CharField(max_length=255, unique=True)
     display_order = models.PositiveSmallIntegerField(default=0)
     def __str__(self):
-        return self.name    
-        
+        return self.name
+
 class WordContent(models.Model):
-    author = models.ForeignKey(User, related_name='word_entries_content') 
+    author = models.ForeignKey(User, related_name='word_entries_content')
     content = models.TextField(blank=False)
     created = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True) 
+    last_modified = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.content
 
 class Picture(models.Model):
-    description = models.CharField(max_length=155)    
+    description = models.CharField(max_length=155)
     file_name = models.CharField(max_length=255)
     display_order = models.PositiveSmallIntegerField(default=0)
     word_content = models.ForeignKey(WordContent, related_name='pictures')
@@ -46,7 +46,7 @@ class WordEntry(models.Model):
     author = models.ForeignKey(User, related_name='word_entries')
     relative_url = models.CharField(max_length=155, unique=True, null=False)
     word = models.CharField(max_length=100, null=False)
-    short_description = models.CharField(max_length=155, null=False)
+    short_description = models.CharField(max_length=255, null=False)
     languages = models.ManyToManyField(Language, related_name='word_entries')
     word_classes = models.ManyToManyField(WordClass, related_name='word_entries')
     audio_file = models.CharField(max_length=255, null=True, blank=True)
@@ -55,24 +55,23 @@ class WordEntry(models.Model):
     template = models.CharField(max_length=50, null=False, default='word-entry-default.html')
     is_published = models.BooleanField(default=False)
     access_count = models.BigIntegerField(default=0, null=False)
-    word_content = models.ForeignKey(WordContent, related_name='word_entries',null=True) 
+    word_content = models.ForeignKey(WordContent, related_name='word_entries',null=True)
     words_related = models.ManyToManyField('WordEntry', related_name='word_related_to')
     created = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True) 
+    last_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.word + ' (url: /' + self.relative_url + ')' 
+        return self.word + ' (url: /' + self.relative_url + ')'
 
-    
+
 class Comment(models.Model):
-    author = models.ForeignKey(User, null=True, related_name='word_entry_comments') 
-    word_entry = models.ForeignKey(WordEntry, related_name='word_entry_comments') 
+    author = models.ForeignKey(User, null=True, related_name='word_entry_comments')
+    word_entry = models.ForeignKey(WordEntry, related_name='word_entry_comments')
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-    is_approved = models.BooleanField(default=False)        
+    is_approved = models.BooleanField(default=False)
     def __str__(self):
         if self.author:
             return self.author.get_full_name()
         return 'Anonymous'
-        
