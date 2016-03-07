8e286683c08ce5077934f8a500291d9be6214cfd
JB <juliano.binder@gmail.com>
2016-03-07 21:28:42 +0000

    ip thing

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='nirvaris-dictionary',
-    version='0.8',
+    version='0.9',
     packages=['dictionary'],
     include_package_data=True,
     license='MIT License',  # example license
